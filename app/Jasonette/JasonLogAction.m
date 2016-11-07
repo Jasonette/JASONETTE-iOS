44496466166c039d6a69fd4e641c76e9cde14b71
Stefan Eletzhofer <se@nexiles.de>
2016-11-07 17:56:10 +0100

    Log Action
    Renamed input option from "message" to "text" for consistency with
    other actions.

--- app/Jasonette/JasonLogAction.m
+++ app/Jasonette/JasonLogAction.m
@@ -11,14 +11,14 @@
 @implementation JasonLogAction
 - (void)info {
     if(self.options){
-        NSString *message = self.options[@"message"];
+        NSString *message = self.options[@"text"];
         asl_log(NULL, NULL, ASL_LEVEL_INFO, "%s", [message UTF8String]);
     }
     [[Jason client] success];
 }
 - (void)debug {
     if(self.options){
-        NSString *message = self.options[@"message"];
+        NSString *message = self.options[@"text"];
         asl_log(NULL, NULL, ASL_LEVEL_DEBUG, "%s", [message UTF8String]);
         NSLog(@"DEBUG: %@", message);
     }
@@ -26,7 +26,7 @@
 }
 - (void)error {
     if(self.options){
-        NSString *message = self.options[@"message"];
+        NSString *message = self.options[@"text"];
         asl_log(NULL, NULL, ASL_LEVEL_ERR, "%s", [message UTF8String]);
         NSLog(@"ERROR: %@", message);
     }
