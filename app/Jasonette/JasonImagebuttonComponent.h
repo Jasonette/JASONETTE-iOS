3376e824611a09f95959c093807bee5c0499231c
Wajahat Chaudhry <wajahat@croem.net>
2016-12-27 16:27:52 +0500

    ImageButton component added

---
+++ app/Jasonette/JasonImagebuttonComponent.h
@@ -0,0 +1,15 @@
+//
+//  JasonImageButtonComponent.h
+//  Jasonette
+//
+//  Created by Wajahat Chaudhry on 27/12/2016.
+//  Copyright © 2016 Jasonette. All rights reserved.
+//
+
+#import "JasonComponent.h"
+#import "Jason.h"
+#import "JasonLabelComponent.h"
+
+@interface JasonImagebuttonComponent : JasonComponent
+
+@end
