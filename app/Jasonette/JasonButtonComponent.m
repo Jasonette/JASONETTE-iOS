51fce51ac59f513cf30b63d7e1364e139ab02162
Wajahat Chaudhry <wajahat@croem.net>
2016-12-28 10:20:12 +0500

    Image added on Button

--- app/Jasonette/JasonButtonComponent.m
+++ app/Jasonette/JasonButtonComponent.m
@@ -11,8 +11,44 @@
     if(!component){
         component = [[UIButton alloc] init];
     }
+    if(json[@"url"]){
+        UIImage *placeholder_image = [UIImage imageNamed:@"placeholderr"];
+        [component setBackgroundImage:placeholder_image forState:UIControlStateNormal];
+        NSString *url = (NSString *)[JasonHelper cleanNull: json[@"url"] type:@"string"];
+        
+        
+        SDWebImageDownloader *manager = [SDWebImageManager sharedManager].imageDownloader;
+        NSDictionary *session = [JasonHelper sessionForUrl:url];
+        if(session && session.count > 0 && session[@"header"]){
+            for(NSString *key in session[@"header"]){
+                [manager setValue:session[@"header"][key] forHTTPHeaderField:key];
+            }
+        }
+        if(json[@"header"] && [json[@"header"] count] > 0){
+            for(NSString *key in json[@"header"]){
+                [manager setValue:json[@"header"][key] forHTTPHeaderField:key];
+            }
+        }
+        
+        UIImageView * btnImage = [[UIImageView alloc] init];
+        
+        if(![url containsString:@"{{"] && ![url containsString:@"}}"]){
+            [btnImage sd_setImageWithURL:[NSURL URLWithString:url] placeholderImage:placeholder_image completed:^(UIImage *i, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+                if(!error){
+                    JasonComponentFactory.imageLoaded[url] = [NSValue valueWithCGSize:i.size];
+                    [component setBackgroundImage:btnImage.image forState:UIControlStateNormal];
+                }
+            }];
+        }
+        [component setTitle:@"" forState:UIControlStateNormal];
+        
+    }
+    else{
+        [component setBackgroundImage:nil forState:UIControlStateNormal];
+        [component setTitle:json[@"text"] forState:UIControlStateNormal];
+    }
     [self stylize:json component:component];
-    [component setTitle:json[@"text"] forState:UIControlStateNormal];
+    
     if(json[@"action"]){
         component.payload = [@{@"action": json[@"action"]} mutableCopy];
     }
