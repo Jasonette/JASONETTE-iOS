c4b2296519a17cf839273c012da8d1a3944c6283
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-04-03 17:21:17 -0400

    Html container level event handling

--- app/Jasonette/JasonHtmlComponent.h
+++ app/Jasonette/JasonHtmlComponent.h
@@ -5,6 +5,7 @@
 //  Copyright © 2016 gliechtenstein. All rights reserved.
 //
 #import "JasonComponent.h"
+#import "Jason.h"
 
 @interface JasonHtmlComponent : JasonComponent
 @end
--- app/Jasonette/JasonHtmlComponent.m
+++ app/Jasonette/JasonHtmlComponent.m
@@ -41,16 +41,33 @@
     // user interaction enable/disable => disabled by default
     component.userInteractionEnabled = NO;
     if(json[@"action"]){
+        // if there's an 'action' attribute, delegate the event handling to this component
+        component.userInteractionEnabled = YES;
+
         NSString *type = json[@"action"][@"type"];
-        if(type){
-            if([type isEqualToString:@"$default"]){
-                // enable input only when action type is $default
-                component.userInteractionEnabled = YES;
-            }
+        if(type && [type isEqualToString:@"$default"]){
+            // don't add button so the event passes all the way to the web canvas
+        } else {
+            // add a button so the event gets processed without reaching the web canvas
+            UIButton *b = [UIButton buttonWithType:UIButtonTypeCustom];
+            b.frame = CGRectMake(0, 0, component.frame.size.width, component.frame.size.height);
+            [component addSubview:b];
+            
+            b.payload = [@{@"action": json[@"action"]} mutableCopy];
+            [b removeTarget:self.class action:@selector(actionButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+            [b addTarget:self.class action:@selector(actionButtonClicked:) forControlEvents:UIControlEventTouchUpInside];            
         }
+        
+
     }
     
     return component;
+}
++ (void)actionButtonClicked:(UIButton *)sender{
+    NSLog(@"sender.payload = %@", sender.payload);
+    if(sender.payload && sender.payload[@"action"]){
+        [[Jason client] call: sender.payload[@"action"]];
+    }
 }
 
 
