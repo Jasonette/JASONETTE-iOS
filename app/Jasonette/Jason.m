2fea76811f91b3e6b8899f344760cfc517a1048f
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-11-04 06:40:37 -0700

    Websocket (#291)
    * added $websocket
    
    * clear websocket everytime a view gets attached
    
    * added comments

--- app/Jasonette/Jason.m
+++ app/Jasonette/Jason.m
@@ -1248,6 +1248,10 @@
     [self.avCaptureSession stopRunning];
     self.avCaptureSession = nil;
     
+    if(self.services[@"JasonWebsocketService"]) {
+        [self.services[@"JasonWebsocketService"] close];
+    }
+    
     JasonMemory *memory = [JasonMemory client];
     
     if(memory.executing){
