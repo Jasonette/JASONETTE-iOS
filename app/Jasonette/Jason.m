2e716f484bc29ac44709bd19c2ea1b093b8493d9
Kevin Spain <kevin.spain@finalsite.com>
2019-01-09 15:19:21 -0500

    APP-32 Changes to support having a `home.json` file again and using it as an offline first, home page that also shares config information between the os's

--- app/Jasonette/Jason.m
+++ app/Jasonette/Jason.m
@@ -826,7 +826,10 @@
     
     // The default template is 'body'
     NSString *template_name = @"body";
-    
+    @try {
+        // If the request returned "head_styles" we want to update the model to be able to use them for rendering.
+        [VC setValue:VC.data[@"$get"][@"head_styles"] forKey:@"style"];
+    } @catch (NSException *exception) { }
     if(stack[@"options"] && stack[@"options"][@"template"]){
         /**************************************************
          *
