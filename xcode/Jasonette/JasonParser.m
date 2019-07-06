b4dc2455b759d2d604f75399f479bb124f910f21
Camilo <camilo@ninjas.cl>
2019-07-06 14:23:34 -0400

    added logger to JasonParser.m

--- xcode/Jasonette/JasonParser.m
+++ xcode/Jasonette/JasonParser.m
@@ -5,10 +5,12 @@
 //  Copyright © 2016 gliechtenstein. All rights reserved.
 //
 #import "JasonParser.h"
-#define NSLog(FORMAT, ...) printf("%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+#import "JasonLogger.h"
 
 @implementation JasonParser
-- (void)format{
+
+- (void)format
+{
     NSDictionary *data = self.options[@"data"];
     id schema = self.options[@"template"];
     if(data && data.count > 0){
@@ -21,7 +23,7 @@
         
         JSContext *context = [[JSContext alloc] init];
         [context setExceptionHandler:^(JSContext *context, JSValue *value) {
-            NSLog(@"%@", value);
+            DTLogWarning(@"%@", value);
         }];
 
         [context evaluateScript:js];
@@ -44,10 +46,17 @@
         [[Jason client] success];
     }
 }
-+ (id)parse: (id)data with: (id)parser{
+
++ (id) parse: (id)data
+       with: (id)parser
+{
     return [self parse:data type:@"json" with:parser];
 }
-+ (id)parse: (id)data type: (NSString *) type with: (id)parser{
+
++ (id)parse: (id)data
+       type: (NSString *) type
+       with: (id)parser
+{
     if(type && [[type lowercaseString] isEqualToString:@"html"]){
         if(data && [data count] > 0){
             NSString *str = data[@"$jason"];
@@ -60,7 +69,7 @@
             
             JSContext *context = [[JSContext alloc] init];
             [context setExceptionHandler:^(JSContext *context, JSValue *value) {
-                NSLog(@"%@", value);
+                DTLogWarning(@"%@", value);
             }];
 
             [context evaluateScript:js];
@@ -103,7 +112,7 @@
             
             JSContext *context = [[JSContext alloc] init];
             [context setExceptionHandler:^(JSContext *context, JSValue *value) {
-                NSLog(@"%@", value);
+                DTLogWarning(@"%@", value);
             }];
 
             [context evaluateScript:js];
@@ -162,12 +171,12 @@
                 data = mutable_data;
             }
             [context setExceptionHandler:^(JSContext *context, JSValue *value) {
-                NSLog(@"%@", value);
+                DTLogWarning(@"%@", value);
             }];
             
             [context evaluateScript:@"var console = {}"];
             context[@"console"][@"log"] = ^(NSString *message) {
-                NSLog(@"Javascript log: %@",message);
+                DTLogDebug(@"JS: %@",message);
             };
 
 
