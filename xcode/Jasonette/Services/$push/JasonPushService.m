46e6cc1ea0cb7e469f3bb8b1e56efadbb74c4130
Camilo <camilo@ninjas.cl>
2019-07-13 20:39:54 -0400

    updated JasonPushService

--- xcode/Jasonette/Services/$push/JasonPushService.m
+++ xcode/Jasonette/Services/$push/JasonPushService.m
@@ -67,12 +67,22 @@
 }
 
 - (void)onRemoteNotificationDeviceRegistered:(NSNotification *)notification {
+    
     NSDictionary * payload = notification.userInfo;
     NSDictionary * events = [[[Jason client] getVC] valueForKey:@"events"];
 
     if (events) {
         if (events[@"$push.onregister"]) {
-            [[Jason client] call:events[@"$push.onregister"] with:@{ @"$jason": @{ @"token": payload[@"token"] } }];
+            
+            NSDictionary * params = @{ @"$jason":
+                                        @{ @"token":
+                                               payload[@"token"]
+                                           }
+                                    };
+            
+            DTLogDebug(@"Calling $push.onregister event with params %@", params);
+            
+            [[Jason client] call:events[@"$push.onregister"] with:params];
         }
     }
 }
@@ -84,16 +94,26 @@
     completionHandler (UNNotificationPresentationOptionNone);
 }
 
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler {
+- (void) userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
+    
     if (response.notification.request.content.userInfo) {
+        
+        DTLogDebug(@"Received Notification Response %@", response.notification.request.content.userInfo);
+        
         if (response.notification.request.content.userInfo[@"href"]) {
+            
+            DTLogDebug(@"Show href");
             [[Jason client] go:response.notification.request.content.userInfo[@"href"]];
+            
         } else if (response.notification.request.content.userInfo[@"action"]) {
+            
+            DTLogDebug(@"Executing Action");
             [[Jason client] call:response.notification.request.content.userInfo[@"action"]];
         }
     }
-
+    
     completionHandler ();
+
 }
 
 @end
