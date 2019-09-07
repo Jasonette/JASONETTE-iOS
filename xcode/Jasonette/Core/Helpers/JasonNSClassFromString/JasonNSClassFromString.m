6bff3535820068a2b06e4a8a69507a748d21c107
Camilo <camilo@ninjas.cl>
2019-09-07 12:38:53 -0400

    improved classfromstring

--- xcode/Jasonette/Core/Helpers/JasonNSClassFromString/JasonNSClassFromString.m
+++ xcode/Jasonette/Core/Helpers/JasonNSClassFromString/JasonNSClassFromString.m
@@ -18,26 +18,35 @@
         DTLogWarning(@"Empty className given");
         className = @"";
     }
-
+    
     Class class = NSClassFromString (className);
-
-    if (!class) {
-        NSString * prefix = [[NSBundle mainBundle] infoDictionary][@"CFBundleExecutable"];
-        // Swift Classes contains the bundle executable as prefix
-        className = [NSString stringWithFormat:@"%@.%@", prefix, className];
-        class = NSClassFromString (className);
-    }
     
     if(!class) {
         // Search in the services for a lowercase string className
+        DTLogDebug(@"Searching class %@ in extensions as %@", className, [className lowercaseString]);
         class = [[Jason client].services[[className lowercaseString]] class];
     }
-
+    
+    if (!class) {
+        NSString * prefix = [[NSBundle mainBundle] infoDictionary][@"CFBundleExecutable"];
+        // Swift Classes contains the bundle executable as prefix
+        NSString * swiftClassName = [NSString stringWithFormat:@"%@.%@", prefix, className];
+        
+        DTLogDebug(@"Searching class %@ in Swift Format %@", className, swiftClassName);
+        class = NSClassFromString (swiftClassName);
+        
+        if(!class) {
+            class = [[Jason client].services[[swiftClassName lowercaseString]] class];
+        }
+    }
+    
     if (!class) {
         DTLogWarning (@"Class %@ not found", className);
         class = nil;
+    } else {
+        DTLogDebug(@"Class %@ found", NSStringFromClass(class));
     }
-
+    
     return class;
 }
 
