95ffe9a8473fe6ef1be6074a1dd389913a4e587a
Kevin Spain <kevin.spain@finalsite.com>
2020-01-07 15:10:28 -0500

    APP-324 Made it so that the screenshot ui tests for android and iOS set flags to set the screenshot emulators locations to the first 20 available for the client. Decided to fix it this way so that we wouldn't have to put any code into the composer side to try and handle something that is so infrequently required. If a client somehow has no content for the first 20 locations then deployment still has the option to take screenshots by temporarily disabling locations filtering on the app However, this should handle the vast majority of cases automatically for them.

--- app/FinalsiteUITests/FinalsiteUITests.swift
+++ app/FinalsiteUITests/FinalsiteUITests.swift
@@ -15,14 +15,16 @@ class FinalsiteUITests: XCTestCase {
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+        // iPad screenshots sometimes decide to fail when switching tabs, if they fail once they will fail on all retries so we want to prevent retrying the test by setting this flag to true - in testing this took 3 minutes off of my screenshot generation time
+        continueAfterFailure = true
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         
         let app = XCUIApplication()
         setupSnapshot(app)
+
+        app.launchArguments = ["-for-screenshots"]
         app.launch()
-        
+
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
--- app/Jasonette/Jason.m
+++ app/Jasonette/Jason.m
@@ -109,6 +109,14 @@
      * (ex) [[Jason client] start] from JasonAppDelegate
      *
      **************************************************/
+     if ([[NSProcessInfo processInfo].arguments containsObject:@"-for-screenshots"]) {
+         [self call:@{
+           @"type": @"$global.set",
+           @"options": @{
+             @"locations": @"1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20"
+           }
+         }];
+     }
     JasonAppDelegate *app = (JasonAppDelegate *)[[UIApplication sharedApplication] delegate];
     NSDictionary *plist = [self getSettings];
     ROOT_URL = plist[@"url"];
